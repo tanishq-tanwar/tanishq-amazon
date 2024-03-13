1e3feb39d17b0151782f6a72cd3f64758e0dab0a
tanishq-tanwar <137075721+tanishq-tanwar@users.noreply.github.com>
2024-03-13 22:41:58 +0530

    Update Menubar.jsx

--- src/Menubar.jsx
+++ src/Menubar.jsx
@@ -1,12 +1,19 @@
 import { NavLink } from "react-router-dom";
 import './App.css'
+import { useContext } from "react";
+import { MyContext } from "./Layout";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Menubar = () => {
+    const {cart} = useContext(MyContext) 
+
     return (
         <>
             <NavLink to='/' className="link">About</NavLink> &nbsp;&nbsp;&nbsp;
             <NavLink to='/products' className="link">Products</NavLink> &nbsp;&nbsp;&nbsp;
-            <NavLink to='/cart' className="link">Cart</NavLink> &nbsp;&nbsp;&nbsp;
+            <NavLink to='/cart' className="link position-relative">Cart
+            <p className="position-absolute placement-sm">{cart.length}</p>
+            </NavLink> &nbsp;&nbsp;&nbsp;
         </>
     )
 }
